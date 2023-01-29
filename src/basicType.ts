f1811304056f8427fb4b0cf4af34c0ca8407171d
takuma-wada <9th.takuma@gmail.com>
2023-01-29 15:38:22 +0900

    :alembic: add TS basic type practie

--- src/basicType.ts
+++ src/basicType.ts
@@ -1,2 +1,77 @@
 // 基本的な型
-console.log('basic')
+console.log("----- array型 -----"); //配列
+const hobbies = ["Sports", "Cooking"];
+for (const hobby of hobbies) {
+    console.log(hobby);
+}
+
+let favoriteHobbies: string[];
+favoriteHobbies = ["Sports"];
+
+console.log("----- tuple型 -----"); // 長さ固定の配列　 goの配列のイメージ
+let tuple: [string, number];
+tuple = ["tuple", 10];
+
+console.log("----- enum型 -----"); //多言語と同じ列挙型
+// デフォルトでは0から番号が割り振られる。意図的にlabelを追加もできる(文字列可)
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR,
+}
+console.log(Role.ADMIN);
+
+console.log('----- any型 -----'); //最後の手段。typescript使っている以上は使うのは微妙
+let anyType: any;
+
+console.log('----- union型 -----'); //型に複数の可能性を持たせる
+function combine(input1: number | string, input2: number | string): number | string //戻り値も指定可能
+{
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+
+    return result;
+}
+
+console.log('----- リテラル型 -----'); // 型に値を指定するイメージ
+let literalType: 'johnDoe';
+// literalType = '100'; => エラーになる
+let unionLiteralType: 'john' | 'Doe'; // union型との組み合わせ
+unionLiteralType = 'john';
+unionLiteralType = 'Doe';
+// unionLiteralType = 'johnDoe'; => エラーになる
+
+console.log('----- 型エイリアス -----'); // 型に値を指定するイメージ
+type stringAlias = string | number;
+
+//使いまわせる
+let type1: stringAlias;
+let type2: stringAlias;
+
+
+console.log('----- function型 -----');
+let combineValues: (a: number | string, b: number | string) => number | string
+combineValues = combine;
+combineValues(100, 200);
+
+console.log('----- function型 callback -----');
+function addAndHandler(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+}
+addAndHandler(10, 20, (result) => {
+    console.log(result);
+})
+
+console.log('----- never型 -----');
+function generateError(message: string, code: number): never {
+    throw {
+        message: message,
+        errorcode: code
+    }
+}
+
+const result = generateError('エラー発生', 1);
